c6ece2c5f3c81d653f4544f9da2025ee441efb37
James Allen <james_allen2@baylor.edu>
2022-05-05 19:57:35 -0500

    maybe a 50

--- MainApp/models.py
+++ MainApp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Pizza(models.Model):
     pizza_name = models.CharField(max_length=200)
+    
 
     def __str__(self):
         return self.pizza_name
@@ -13,3 +14,10 @@ class Topping(models.Model):
     
     def __str__(self):
         return f"{self.topping_name[:50]}..."
+
+class Comment(models.Model):
+    Pizza = models.ForeignKey(Pizza,on_delete=models.CASCADE)
+    comment_entry = models.TextField()
+
+    def __str__(self):
+        return f"{self.comment_entry[:50]}..."
